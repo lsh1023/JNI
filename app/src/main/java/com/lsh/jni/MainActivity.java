76e54774f2f97fdc94569ba1fca1a805c581111d
LSH <lisonghua1023@163.com>
2017-09-12 14:22:40 +0800

    JNI的测试

--- app/src/main/java/com/lsh/jni/MainActivity.java
+++ app/src/main/java/com/lsh/jni/MainActivity.java
@@ -3,6 +3,9 @@ package com.lsh.jni;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * JNI的测试
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
